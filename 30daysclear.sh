3cfcfa9cd01220fe129aa1574bd44cb98b6bfe5e
stefantoczek <stetoc37@gmail.com>
2018-10-16 00:34:30 +0200

    anacrontab creator

--- 30daysclear.sh
+++ 30daysclear.sh
@@ -1,2 +1,2 @@
-find ~/Downloads/ -maxdepth 1 -type f -mtime +30 -exec rm {} \;
-find ~/Downloads/ -maxdepth 1 -type d -mtime +30 -exec rm -rf {} \;
+echo "@monthly		5	remove_month_downloads.files	find ~/Downloads/ -maxdepth 1 -type f -mtime +30 -exec rm {} \;" >> /etc/anacrontab
+echo "@monthly		5	remove_month_downloads.directories	find ~/Downloads/ -maxdepth 1 -type d -mtime +30 -exec rm -rf {} \;" >> /etc/anacrontab
